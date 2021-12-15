b83320ba773cb153a88cdc05bf742b6ae2b6a11d
Gobuub <botica1982@msn.com>
2021-12-15 17:20:22 +0100

    Añadida ultima querie

---
+++ Queries/Ejercicio5.sql
@@ -0,0 +1,46 @@
+select older_emp.Title as Title,
+older_emp.Employees as Employees, 
+Former_Employee, New_Employee
+from (select ehd.Title as Title, 
+concat(employees.first_name, ' ', employees.last_name) as Former_Employee,
+Employees
+from
+(select titles.title as Title, 
+count(distinct(employees.emp_no)) As Employees,
+min(titles.from_date) as Min_from_date,
+max(titles.from_date) as Max_from_date,
+min(employees.hire_date) as Min_hire_date,
+max(employees.hire_date) as Max_hire_date 
+from employees
+join titles on employees.emp_no = titles.emp_no
+group by Title ) as ehd
+
+join titles on ehd.Title = titles.title
+join employees on titles.emp_no = employees.emp_no
+
+where titles.from_date = ehd.Min_from_date 
+and titles.title = ehd.Title 
+group by ehd.Title
+order by from_date) as older_emp 
+
+join (select ehd.Title as Title, 
+concat(employees.first_name, ' ', employees.last_name) as New_Employee
+from
+(select titles.title as Title, 
+count(distinct(employees.emp_no)) As Employees,
+min(titles.from_date) as Min_from_date,
+max(titles.from_date) as Max_from_date,
+min(employees.hire_date) as Min_hire_date,
+max(employees.hire_date) as Max_hire_date 
+from employees
+join titles on employees.emp_no = titles.emp_no
+group by Title ) as ehd
+
+join titles on ehd.Title = titles.title
+join employees on titles.emp_no = employees.emp_no
+
+where titles.from_date = ehd.Max_from_date 
+and titles.title = ehd.Title 
+group by ehd.Title
+order by from_date) as new_emp on older_emp.Title = new_emp.Title
+;
